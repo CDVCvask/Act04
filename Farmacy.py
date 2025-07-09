7275bea6a98f626b6bf90325b0d1718ee3237d9f
CDVCvask <cesarcamposeco415@gmail.com>
2025-07-09 10:51:32 -0600

    Principio medicamentos

--- Farmacy.py
+++ Farmacy.py
@@ -3,3 +3,21 @@ class Medicine:
         self.name = name
         self.price = price
         self.quantity = quantity
+def Medicines():
+    allow = False
+    medicines = []
+    try:
+        while allow == False:
+            name = input("Ingrese el nombre de la medicina: ")
+            price = int(input("Ingrese el precio de la medicina"))
+            if price <= 0:
+                print("El precio ingresado no es valido")
+            else:
+                quantity = int(input("Ingrese cúantas unidades de esta medicina va a ingresar"))
+                if quantity <= 0:
+                    print("La cantidad ingresada no es valida")
+                else:
+                    medicine = Medicine(name, price, quantity)
+                    medicines.append(medicine)
+    except(ValueError):
+        print("No se a ingresado el tipo de valor correcto")
