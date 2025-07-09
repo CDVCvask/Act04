2dd125507b03b43e09db8418893b2a466b31a68f
ALUMNO <jm.gcaxaj@udeo.edu.gt>
2025-07-09 10:54:53 -0600

    Crear clase y menú

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Inicio")
+class Paciente:
+    def __init__(self, nombre, motivo_consulta):
+        self.nombre = nombre
+        self.motivo_consulta = motivo_consulta
+    def ver_cliente(self):
+        print(f"Nombre: {self.nombre}, motivo de consulta: {self.motivo_consulta}")
+
+opcion = 0
+while opcion != "4":
+    print("==MENÚ DE RECEPCIÓN==")
+    print("1.Registrar paciente")
+    print("2.Atender paciente")
+    print("3.Mostrar pacientes en espera")
+    print("4.Salir")
+    try:
+        opcion = input("\nSeleccione una opción: ")
+
+    except ValueError:
+        print("ERROR: Dato ingresado no válido")
